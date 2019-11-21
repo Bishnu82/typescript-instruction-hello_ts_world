a0ac66732bf91575b0613b6905a4d0de0cb46764
John Cawley <jcawleyiii@gmail.com>
2019-11-21 10:11:17 -0500

    first init

---
+++ js/hello_world.js
@@ -0,0 +1,4 @@
+"use strict";
+var hw = "Hello, world";
+var nameStr = "J Sizzle";
+console.log(hw, nameStr);
---
+++ ts/hello_world.ts
@@ -0,0 +1,3 @@
+let hw:string = "Hello, world";
+let nameStr:string = "J Sizzle";
+console.log(hw, nameStr);
